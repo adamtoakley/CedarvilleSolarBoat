d7f0b78f06b77a75d3bc2ba2d24dc1c8667e1fd2
adamtoakley <adamtoakley@yahoo.com>
2015-10-14 21:43:54 -0400

    Updated Donate page and home styles

--- SolarBoat/donate.php
+++ SolarBoat/donate.php
@@ -1,15 +1,20 @@
 <?php include('head.php') ?>
 <?php include('navigation.php') ?>
 <div class="wrapper-main">
-            
+
     <section class="featured">
         <div class="content-wrapper">
             <hgroup class="title">
                 <h1>Donate</h1>
             </hgroup>
+            <div style="text-align:right">
+                <a class="logo-wrapper" href="http://cedarville.edu/SolarBoatGive" target="_blank">
+                    <img src="Content/images/logo-donate.png" alt="Cedarville Solar Boat Team" />
+                </a>
+            </div>
         </div>
     </section>
-	
-	
-	<?php include('footer.php') ?>
+
+
+    <?php include('footer.php') ?>
 </div>
--- SolarBoat/head.php
+++ SolarBoat/head.php
@@ -5,11 +5,11 @@
         <title>Cedarville Solar Boat Team</title>
         <link href="Content/Content/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
 		<link href='http://fonts.googleapis.com/css?family=Exo:400,600' rel='stylesheet' type='text/css'>
-        <link href="Content/Site.css" rel="stylesheet"/>
-        <link href="Content/solar-styles.css" rel="stylesheet" />
 
         <link href="Content/jquery.lightbox-0.5.css" rel="stylesheet"/>
         <link href="Content/bootstrap.css" rel="stylesheet"/>
+        <link href="Content/Site.css" rel="stylesheet" />
+        <link href="Content/solar-styles.css" rel="stylesheet" />
         <script src="Scripts/modernizr-2.5.3.js"></script>
 
         <script src="Scripts/jquery-1.9.1.js" type="text/javascript"></script>

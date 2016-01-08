2c42bcdebd615582ea6b6adc198a8f6b57351edc
Adam Oakley <aoakley@hmbnet.com>
2016-01-08 01:09:07 -0500

    Updating Homepage

--- SolarBoat/index.php
+++ SolarBoat/index.php
@@ -1,7 +1,7 @@
 <?php include('head.php') ?>
 <?php include('navigation.php') ?>
 <div class="wrapper-main">
-            
+
     <section class="featured">
         <div class="content-wrapper">
             <hgroup class="title">
@@ -11,13 +11,67 @@
     </section>
     <section class="content-wrapper main-content clear-fix">
         <p>
-        The Cedarville University Solar Boat Team is fresh off impressive performances both in the US and abroad. In July 2012, the Cedarville team was the <a href="http://www.cedarville.edu/Offices/Public-Relations/CampusNews/2012/Solar-Team-Make-Splash-in-Netherlands.aspx" target="_blank">top university team in the Dong Energy Solar Challenge (the Netherlands)</a> finishing ahead of all other university teams and taking a 3rd place overall in the Top Class. In 2013, Cedarville captured the <a href="http://www.solarsplash.com" target="_blank">Solar Splash World Championship of Collegiate Solar Boating</a>; this is Cedarville’s seventh championship in the past 10 years.</p>
-        <p>The Dong Energy Solar Challenge, with over 40 boats participating, follows the 220 km course of the great Elfstedentocht, a famous ice skating race around the province of Friesland, the Netherlands. The course begins and ends in Leeuwarden, the capital city of Friesland,  and traverses the Frisian canals and lakes, passing through eleven cities. The 2012 race was a challenge in perseverance as rain continually hampered the event and forced teams to be especially diligent in the management of energy. The Cedarville team was the only team from the US and, in addition to defeating the university teams from Europe, Cedarville also beat teams from Brazil, Turkey, and China. Two boats, out of the same mold, finished ahead of Cedarville. These boats, sponsored by Private Energy, were impressive in their performance and set a standard for other boats to strive to achieve.</p>
-        <p>In the rain-shortened Solar Splash 2013 competition, Cedarville took 1st place in the Endurance, Sprint, and Slalom, and 3rd in the Qualifier.  Cedarville also took 1st place in the Tech Report and 2nd in the Technical Display.</p>
-        <p>Starting in 2014, Cedarville will host the Solar Splash in Dayton, Ohio.  This is a great opportunity for teams to visit Dayton, the City of Inventions.  The Wright Brothers were based here and minutes away from the race venue are numerous aviation landmarks.  Nearby Huffman Prairie is where the Wright Brothers spent years improving their plane, and a replica of their hanger and launch system are open to public viewing.  The National Museum of the United States Air Force, less than a mile away, hosts over a million visitors per year.</p>
-        <p>Dayton was also once a major race venue for Thunderboat racing.  Some of the fastest boats in the world (e.g. Miss Budweiser, Miss Madison, Oh Boy Oberto, etc.) would compete in the Dayton Hydrobowl, now called Eastwood Lake, while on the race circuit.  Eastwood Lake in Eastwood Park is the venue for Solar Splash 2014.  Dayton’s history of invention and boat racing make this an ideal venue for Solar Splash.  Cedarville University’s Solar Boat Team has a proven track record of world-class status and we look forwarding to hosting this event. </p>
-        <p>None of our success would be possible without the grace of God; we are greatly blessed by a great team of sponsors who have been extremely generous to us.   Our success is a gift of God, but when "it’s all been said and done," "you can take this whole world (and all its success), but give me Jesus."</p>
-        <p>For more information on Cedarville University's Solar Boat Team, please <a href="mailto:dewhurst@cedarville.edu">contact Dr. Timothy Dewhurst</a>.</p>
+            The highly successful Cedarville University Solar Boat Team is teaming up with SeaLandAire
+            Technologies to take the next step in accepting the
+            <a href="http://www.dutchsolarchallenge.nl/en/">Dutch Solar Challenge.</a>
+        </p>
+        <p>
+            Cedarville University is fresh off its 8th
+            <a href="http://solarsplash.com/">Solar Splash World Championship</a>
+            and is eager to return to the Dutch Solar Challenge in the Netherlands where in 2012 Cedarville
+            was the top university team finishing ahead of all other schools while taking a 3rd place overall
+            in the Top Class.
+        </p>
+        <p>
+            <a href="http://sealandaire.com/">SeaLandAire Technologies, Inc.</a>
+            is a vibrant, innovative company that has created its reputation as a world-leader in
+            <a href="http://sealandaire.com/Product/hydrophones/">hydrophones</a>
+            while developing impressive projects such as the
+            <a href="http://sealandaire.com/project/phase/">PHASE</a>,
+            <a href="http://sealandaire.com/project/compact-riverine-auv/">the Compact Rivervine AUV</a>,
+            the <a href="http://sealandaire.com/project/cmas/">CMAS</a> (for which the Cedarville boat team
+            designed and built the propellers), the <a href="http://sealandaire.com/project/pos-skb/">POS-SKB</a>
+            and many other technically challenging products for scientific and military purposes.
+            Two important alumni of Cedarville University’s Solar Boat Team are now key members of SeaLandAire’s
+            engineering development team.
+        </p>
+        <p>
+            The 2016 Dutch Solar Challenge, with over 30 boats participating from 9 different countries, will
+            start in Amsterdam.  The race will then proceed through the canals and lakes of Friesland, and
+            will end in Leeuwarden, home of the great Elfstedentocht, a famous ice skating race around the
+            province. The 2012 race was a challenge in perseverance as rain continually hampered the event
+            and forced teams to be especially diligent in the management of energy. The Cedarville team was
+            the only team from the US and, in addition to defeating the university teams from Europe,
+            Cedarville also beat teams from Brazil, Turkey, and China. Two boats, out of the same mold,
+            finished ahead of Cedarville. These boats, sponsored by Clafis/Private Energy, were impressive
+            in their performance and set a standard for other boats to strive to achieve. TU Delft, multiple
+            winner of the <a href="http://www.worldsolarchallenge.org/">World Solar Challenge (Australia)</a> 
+            and the Dutch Solar Challenge is always a strong, innovative contender, as is the Midnight Sun
+            boat of KYAMK University in Finland.
+        </p>
+        <p>
+            Since 2014, Cedarville has hosted the Solar Splash in Dayton, Ohio, the City of Inventions. The
+            Wright Brothers were based here and minutes away from the race venue are numerous aviation
+            landmarks. Nearby Huffman Prairie is where the Wright Brothers spent years improving their
+            plane, and a replica of their hanger and launch system are open to public viewing. The National
+            Museum of the United States Air Force, less than a mile away, hosts over a million visitors per
+            year.
+        </p>
+        <p>
+            Eastwood Park, formerly known as the Dayton Hydrobowl, was also once a major race venue for
+            Thunderboat racing. Some of the fastest boats in the world (e.g. Miss Budweiser, Miss Madison,
+            Oh Boy Oberto, etc.) would compete at our race venue. Dayton’s history of invention and boat
+            racing make this an ideal venue for Solar Splash.
+            None of our success would be possible without the grace of God; we are greatly blessed by a
+            great team of sponsors who have been extremely generous to us. Our success is a gift of God, but
+            when "it’s all been said and done," "you can take this whole world (and all its success), but give
+            me Jesus."
+        </p>
+        <p>
+            For more information on Cedarville University's Solar Boat Team, please
+            <a href="mailto:dewhurst@cedarville.edu">contact Dr. Timothy Dewhurst</a>
+            .
+        </p>
     </section>
     <?php include('footer.php') ?>
 </div>

5d428b8e8c0e76bc237efd54c917551c2478f049
Adam Oakley <adamtoakley@outlook.com>
2016-04-04 20:38:56 -0400

    Updated Sponsor Links

--- SolarBoat/sponsors.php
+++ SolarBoat/sponsors.php
@@ -13,88 +13,88 @@
         <div class="content-wrapper main-content clear-fix">
             <section class="col-sm-12 sponsors text-center">
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.sealandaire.com/" target="_blank">
                         <img src="Content/images/sponsors/sealandaire_logo.png" alt="sponsor logo" />
                     </a>
 
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
-                        <img src="Content/images/sponsors/solbien_logo.png" alt="sponsor logo" />
+                    <a href="http://www.solbian.eu/" target="_blank">
+                        <img src="Content/images/sponsors/solbian_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="https://genasun.com/" target="_blank">
                         <img src="Content/images/sponsors/genasun_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.ashland.com/" target="_blank">
                         <img src="Content/images/sponsors/ashland_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.airtechintl.com/" target="_blank">
                         <img src="Content/images/sponsors/airtech_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.enersys.com/" target="_blank">
                         <img src="Content/images/sponsors/enersys_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.braider.com/" target="_blank">
                         <img src="Content/images/sponsors/ap_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.lord.com/" target="_blank">
                         <img src="Content/images/sponsors/lord_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="https://www.parker.com" target="_blank">
                         <img src="Content/images/sponsors/parker_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.neweagle.net" target="_blank">
                         <img src="Content/images/sponsors/new_eagle_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.trojon-gear.com/" target="_blank">
                         <img src="Content/images/sponsors/trojon_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.turbocam.com/" target="_blank">
                         <img src="Content/images/sponsors/turbocam_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.alro.com/" target="_blank">
                         <img src="Content/images/sponsors/alro_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.laserlaminations.com/" target="_blank">
                         <img src="Content/images/sponsors/laser_laminations_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.performancepaint.org/" target="_blank">
                         <img src="Content/images/sponsors/performance_paint_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://yamatoracing.com/" target="_blank">
                         <img src="Content/images/sponsors/rpm_marine_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
                 <div class="col-sm-6 sponsor">
-                    <a href="http://site">
+                    <a href="http://www.te.com/" target="_blank">
                         <img src="Content/images/sponsors/te_logo.png" alt="sponsor logo" />
                     </a>
                 </div>
